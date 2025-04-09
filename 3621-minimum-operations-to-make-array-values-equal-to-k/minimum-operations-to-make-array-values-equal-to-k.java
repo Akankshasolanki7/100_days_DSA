f6b7e8fb225127e5e35182ce25ebf8136d1d6c73
Akanksha Solanki <akankshakhushi865@gmail.com>
2025-04-10 00:24:03 +0530

    Time: 3 ms (52.54%) | Memory: 44.5 MB (55.59%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
@@ -1,9 +1,10 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        HashMap<Integer,Integer> mp=new HashMap<>();
-        for(int i:nums)
-        if(i<k) return -1;
-        else if(i>k) mp.put(i, mp.getOrDefault(i,0)+1);
-        return mp.size();
+     HashMap<Integer,Integer> mp=new HashMap<>();
+     for(int i:nums)
+       if(i<k) return-1;
+       else if(i>k)
+       mp.put(i,mp.getOrDefault(1,0)+1);
+       return mp.size();
     }
 }
