4a082963376d8076889733b9940c3a7f4b23f41c
Akanksha Solanki <akankshakhushi865@gmail.com>
2025-05-31 09:44:10 +0530

    Time: 6 ms (45.41%) | Memory: 43.1 MB (83.14%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -5,20 +5,38 @@ class Solution {
         //  Arrays.sort(sarray);
         //  Arrays.sort(tarray);
         //  return Arrays.equals(sarray,tarray);
+        // if(s.length()!=t.length()){
+        //     return false;
+        // }
+        // int[] charcounts=new int[26];
+        // for(int i=0;i<s.length();i++){
+        //     charcounts[s.charAt(i)-'a']++;
+        //     charcounts[t.charAt(i)-'a']--;
+        // }
+        // for(int count:charcounts){
+        //     if(count!=0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+
+
+
         if(s.length()!=t.length()){
             return false;
         }
-        int[] charcounts=new int[26];
+        int [] charfreq= new int[26];
         for(int i=0;i<s.length();i++){
-            charcounts[s.charAt(i)-'a']++;
-            charcounts[t.charAt(i)-'a']--;
+            charfreq[s.charAt(i)-'a']++;
+            charfreq[t.charAt(i)-'a']--;
         }
-        for(int count:charcounts){
+        for(int count:charfreq){
             if(count!=0){
                 return false;
             }
         }
-        return true;
+        return  true;
     }
 }
      
