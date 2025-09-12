22cce67c34b12885275f099cdf62930c7d57f7fe
Akanksha Solanki <akankshakhushi865@gmail.com>
2025-09-13 00:39:04 +0530

    Time: 2 ms (97.75%) | Memory: 43.3 MB (71.46%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,42 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //  char [] sarray=s.toCharArray();
-        //  char [] tarray=t.toCharArray();
-        //  Arrays.sort(sarray);
-        //  Arrays.sort(tarray);
-        //  return Arrays.equals(sarray,tarray);
-        // if(s.length()!=t.length()){
-        //     return false;
-        // }
-        // int[] charcounts=new int[26];
-        // for(int i=0;i<s.length();i++){
-        //     charcounts[s.charAt(i)-'a']++;
-        //     charcounts[t.charAt(i)-'a']--;
-        // }
-        // for(int count:charcounts){
-        //     if(count!=0){
-        //         return false;
-        //     }
-        // }
-        // return true;
+       if(s.length()!=t.length()) return false;
+       int[] freq=new int[26];
+       for(char c:s.toCharArray()){
+        freq[c-'a']++;
+       }
+       for(char c:t.toCharArray()){
+        freq[c-'a']--;
+       }
 
-
-
-
-        if(s.length()!=t.length()){
-            return false;
-        }
-        int [] charfreq= new int[26];
-        for(int i=0;i<s.length();i++){
-            charfreq[s.charAt(i)-'a']++;
-            charfreq[t.charAt(i)-'a']--;
-        }
-        for(int count:charfreq){
-            if(count!=0){
-                return false;
-            }
-        }
-        return  true;
+       for(int count:freq){
+        if(count!=0) return false;
+      
+       }
+         return true;
     }
 }
      
